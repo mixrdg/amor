384f4d00efa7bdbd5f053480affe01936ff3a556
Rodrio Pereira <82533684+mixrdg@users.noreply.github.com>
2024-01-22 22:57:55 -0300

    original

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirCliqueAqui() {
+    document.getElementById('cliqueAqui').style.display = 'inline';
+}
+
+function fecharCliqueAqui() {
+    document.getElementById('cliqueAqui').style.display = 'none';
+}
